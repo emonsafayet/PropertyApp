b763cf63b91693ce18db8ecb713572ee4b4c60ba
Safayet <safayet@labaidpharma.com>
2020-09-13 00:12:35 +0600

    Using Service and HTTP calls in Angular

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
 
 @Component({
   selector: 'app-property-list',
@@ -6,48 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
-  properties: Array<any>=[
-    {
-      "Id" :1,
-      "Name" :"Biral House",
-      "Type":"House",
-      "Price": 12000
-    },
-    {
-      "Id" :2,
-      "Name" :"Erose House",
-      "Type":"House",
-      "Price": 14300
-    },
-    {
-      "Id" :3,
-      "Name" :"Safayet House",
-      "Type":"House",
-      "Price": 14500
-    },
-    {
-      "Id" :4,
-      "Name" :"Shovon House",
-      "Type":"House",
-      "Price": 15000
-    },
-    {
-      "Id" :5,
-      "Name" :"Tahsin House",
-      "Type":"House",
-      "Price": 10000
-    },
-    {
-      "Id" :6,
-      "Name" :"Tahmid House",
-      "Type":"House", 
-      "Price": 19000
-    },
-
-]
-  constructor() { }
+  properties: any;
+  constructor(private housingService:HousingService) { }
 
   ngOnInit(): void {
+    this.housingService.getAllProperties().subscribe(
+      data=>{
+        this.properties =data;
+        console.log(data);
+      },error=>{
+        console.log(error);        
+      }      
+    );
   }
 
 }
