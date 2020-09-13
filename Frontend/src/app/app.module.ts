b9b432e56255480c8e2b75364740cdab84d145be
Safayet <safayet@labaidpharma.com>
2020-09-13 23:48:24 +0600

    add routing

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import{ Routes,RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HousingService } from './services/housing.service';
+import { AddPropertyComponent } from './property/add-property/add-property.component';
+
+const appRoutes :Routes = [
+  {path:'',component:PropertyListComponent},
+  {path:'add-property',component:AddPropertyComponent}
+]
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
     PropertyCardComponent,
     PropertyListComponent,
-      NavBarComponent
+    NavBarComponent,
+    AddPropertyComponent
    ],
   imports: [
-    BrowserModule,HttpClientModule
+    BrowserModule,HttpClientModule,RouterModule.forRoot(appRoutes)
   ],
   providers: [
     HousingService
